edee8fafbdf5b0624e39342f81684fb5872bda34
mike-mendoza <89025728+mike-mendoza@users.noreply.github.com>
2021-10-03 16:27:04 +0200

    Update setup.py
    change version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setuptools.setup(
     name='py_banshee',                           # should match the package folder
     packages=['py_banshee'],                   # should match the package folder
-    version='0.0.0.10',                              # important for updates
+    version='0.0.1',                              # important for updates
     license='GNU',                                  # should match your chosen license
     description='Testing installation of Package',
     long_description=long_description,              # loads your README.md
@@ -31,5 +31,5 @@ setuptools.setup(
         'Programming Language :: Python :: 3.9',
     ],
     
-    download_url="https://github.com/mike-mendoza/py_banshee_test/archive/refs/tags/0.0.0.10.tar.gz",
+    download_url="https://github.com/mike-mendoza/py_banshee_test/archive/refs/tags/v0.0.1.tar.gz",
 )
